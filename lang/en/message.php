c9a9de144d876cdf24b488b14dc14498cc70bfc3
Ismail Dehaoui <81970637+ismailDehaoui@users.noreply.github.com>
2024-07-08 00:20:40 +0100

    Translate  message

---
+++ lang/en/message.php
@@ -0,0 +1,27 @@
+<?php
+
+    return [
+        'invalid_credentials' => 'There is an error in the name or password',
+        'login_success' => 'Login successfully',
+
+        'success' => [
+            'create' => 'Data saved successfully',
+            'update' => 'Data updated successfully',
+            'delete' => 'Data was deleted successfully',
+            'imported' =>'The file was successfully uploaded',
+            "assign" => "Admin has been successfully assigned",
+            'update_bmc' => 'The file was updated successfully',
+        ],
+        'warning' => [
+            'supervisor' => 'Supervisor has been saved. Please add a project.',
+            'project' => 'The project has been saved. Please add a supervisor to the project.'
+        ],
+        'error' => [
+            'create' => 'An error occurred saving data',
+            'update' => 'An error occurred updating data',
+            'delete' => 'An error occurred deleting data',
+            'imported' => 'There was an error uploading the file',
+        ],
+    ]
+
+?>
