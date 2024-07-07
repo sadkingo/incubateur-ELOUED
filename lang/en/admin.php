305a8307f89c284fdfa7ae20a0e1264e82894770
Ismail Dehaoui <81970637+ismailDehaoui@users.noreply.github.com>
2024-07-08 00:19:19 +0100

    Translate  admin

---
+++ lang/en/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+    return [
+        'title' => 'administrators',
+        'dashboard' => 'control panel',
+        'admins' => 'administrators',
+        'welcome' => "Welcome to your control panel",
+
+        'name' => 'name',
+        'phone' => 'phone number',
+        'email' => 'email',
+
+
+        'create'=> 'add admin',
+        'edit' => 'Edit admin data',
+        'delete' => 'delete admin',
+        'delete_confirmation' => 'Are you sure about deleting this admin?',
+        'label' =>[
+            'name' => 'name',
+        ],
+        'placeholder' =>[
+            'name' => 'Search by name',
+        ],
+    ];
+
+?>
