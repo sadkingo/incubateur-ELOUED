ec9c921935796612f1737f022daf464a2fa91f0d
Ismail Dehaoui <81970637+ismailDehaoui@users.noreply.github.com>
2024-07-08 00:24:02 +0100

    Translate  auth/supervisor

---
+++ lang/en/auth/supertvisor.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        'select_supervisor_role' => 'Select supervisory role',
+        'main_supervisor' => 'main_supervisor',
+        'second_supervisor' => 'second supervisor',
+        'assistant_supervisor' => 'assistant_supervisor',
+    ];
+
+?>
