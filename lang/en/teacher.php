c60959feadf31a5f30dc5a4eedf8a8a693f3aeaa
Ismail Dehaoui <81970637+ismailDehaoui@users.noreply.github.com>
2024-07-08 00:22:48 +0100

    Translate teacher

---
+++ lang/en/teacher.php
@@ -0,0 +1,54 @@
+<?php
+
+    return [
+        'title' => 'Professors',
+        'dashboard' => 'control panel',
+        'teachers' => 'professors',
+
+        'name' => 'name',
+        'birthday' => 'date of birth',
+        'gender' => 'sex',
+        'female' => 'female',
+        'male' => 'male',
+        'phone' => 'phone number',
+        'email' => 'email',
+
+        'create' => 'add professor',
+        'edit' => 'Edit professor data',
+        'delete' => 'delete professor',
+        'delete_confirmation' => 'Are you sure you want to delete this professor?',
+        'label' =>[
+            'name' => 'name',
+        ],
+
+        'firstname_fr' => 'Name in French',
+        'lastname_fr' => 'Lastname in French',
+
+        'firstname_ar' => 'Name in Arabic',
+        'lastname_ar' => 'Lastname in Arabic',
+        
+        'select_gender' => 'select gender',
+        'gender_female' => 'female',
+        'gender_male' => 'male',
+        'address' => 'address',
+        'password' => 'password',
+        'confirme_password' => 'confirm password',
+
+
+        'placeholder' => [
+            'name' => 'Search by name',
+
+            'firstname_ar' => 'Enter the name in Arabic',
+            'lastname_ar' => 'Enter last name in Arabic',
+
+            'firstname_fr' => 'Entrez le prenom en français',
+            'lastname_fr' => 'Entrez le family name in français',
+
+            'birthday' => 'date of birth',
+            'gender' => 'sex',
+            'address' => 'Enter address',
+            'phone' => 'Enter phone number',
+            'email' => 'Enter email',
+            'password' => 'password',
+        ],
+    ];
