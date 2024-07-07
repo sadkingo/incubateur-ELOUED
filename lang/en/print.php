fa71098d691e39f2ff8cd89ea29fa1f9f95be3db
Ismail Dehaoui <81970637+ismailDehaoui@users.noreply.github.com>
2024-07-08 00:20:57 +0100

    Translate  print

---
+++ lang/en/print.php
@@ -0,0 +1,161 @@
+<?php
+
+    return [
+
+        'date_print' => 'print date',
+        'project_report' => 'project report',
+        'title' => [
+            'attendence' => 'attendance list of students attending',
+            'trainee_notebook' => 'trainee\'s notebook',
+            'students' => 'list of students',
+            'teachers' => 'List of teachers',
+            'project_report' => 'Print Projects',
+            'commission' => 'print committees',
+        ],
+        'title_print' => [
+            'attendence' => 'print attendance list of students attending',
+            'trainee_notebook' => 'trainee\'s notebook',
+            'students' => 'Print student list',
+            'teachers' => 'Print list of teachers',
+            'project_report' => 'Print Projects',
+            'commission' => 'Print list of committees',
+        ],
+        'weeks' => [
+            '1' => 'first week',
+            '2' => 'second week',
+            '3' => 'third week',
+            '4' => 'fourth week',
+            '5' => 'fifth week',
+        ],
+        'batch' => [
+            'all' => 'all payments',
+            'A' => 'Batch A',
+            'B' => 'Batch B',
+            'C' => 'Batch C',
+        ],
+        'groups' => [
+            'all' => 'all regiments',
+            '1' => 'Cohort 1',
+            '2' => 'Cohort 2',
+            '3' => 'Cohort 3',
+            '4' => 'Cohort 4',
+            '5' => 'Cohort 5',
+            '6' => 'Cohort 6',
+            '7' => 'Cohort 7',
+            '8' => 'Cohort 8',
+        ],
+        'batch' => 'batch',
+        'group' => 'cohort',
+        'ID' => 'number',
+
+        'student' => [
+            'title_ar' => 'Student ID',
+            'title_fr' => 'Identity of the student',
+            'firstname_ar' => 'Name',
+            'lastname_ar' => 'lastname',
+            'firstname_fr' => 'Prénom',
+            'lastname_fr' => 'Nom',
+            'birthday_place_of_birth_ar' => 'date and place of birth',
+            'birthday_place_of_birth_fr' => 'Date and lieu of naissance',
+            'promotion_ar' => 'payment',
+            'promotion_fr' => 'Promotion',
+
+            'branch_ar' => 'branch',
+            'branch_fr' => 'Filière',
+
+            'specialization_ar' => 'specialization',
+            'specialization_fr' => 'Specialité',
+
+            'groupe_ar' => 'group',
+            'groupe_fr' => 'Groupe',
+
+            'matricule_ar' => 'registration number',
+            'matricule_fr' => 'Matricule',
+
+            'phone' => 'phone number',
+            'residence' => 'residence',
+
+            'email' => 'email',
+        ],
+
+        'suivi' => [
+            'title_ar' => 'framing',
+            'title_fr' => 'Suivi de stage',
+            'teacher_charge_ar' => 'The professor supervising the training',
+            'teacher_charge_fr' => 'Engineer charged at home',
+            'person_charged_ar' => 'Assigned to be lurked by the reception institution',
+            'person_charged_fr' => 'Correspondant',
+        ],
+
+        'host_institution' => [
+            'title_ar' => 'Learning Foundation',
+            'title_fr' => 'accueil reception',
+
+            'address_ar' => 'address',
+            'address_fr' => 'Adresse',
+
+            'phone_ar' => 'phone',
+            'phone_fr' => 'Telephone',
+
+            'fax_ar' => 'fax',
+            'fax_fr' => 'Fax',
+
+            'email_ar' => 'email',
+            'email_fr' => 'E-mail',
+
+        ],
+        'duration_of_stage' => [
+            'title_ar' => 'Lurking period',
+            'title_fr' => 'During the stage',
+
+            'duration_date' => 'in the period from',
+            'from_ar' => 'from',
+            'to' => 'to',
+            'from_fr' => 'Au'
+        ],
+
+        'review' => [
+            'title_ar' => 'Final evaluation of the internship (for the supervising professor at the institution level)',
+            'title_fr' => 'Evaluation Finale of the Stage (Reserve to the teacher\'s appointment to the reception)',
+
+            'firstname_lastname_student_ar' => 'Student\'s name and surname',
+            'firstname_lastname_student_fr' => 'Name and study name',
+
+            'firstname_lastname_teacher_ar' => 'Name and surname of the professor',
+            'firstname_lastname_teacher_fr' => 'No and Prénom de l\'enseignant',
+
+            'notes_teacher_ar' => 'Teacher\'s Notes',
+            'notes_teacher_fr' => 'Appréciation de l\'enseignant',
+
+            'ID' => 't.t',
+            'subject_name' => 'Evaluation axes',
+            'coef' => 'coefficient',
+            'rate' => 'point',
+            'total_marks' => 'parameter mark',
+
+            'date_ar' => 'date',
+            'date_fr' => 'Date',
+
+            'signature_ar' => 'Signature of the appointed supervisor',
+            'signature_fr' => 'Signature from correspondan',
+
+            'notes_ar' => 'Note: fill this page without any deletion',
+            'notes_fr' => 'N.B: I don\'t do it because I don\'t charge it',
+        ],
+
+
+        'certificate' => [
+            'title' => 'Certificate of completion of training',
+            'toStudent' => 'To the student',
+        ],
+        'project' => [
+            'accept' => 'accepted projects',
+            'reject' => 'Rejected projects',
+            'understudy' => 'Projects under study',
+            'complte' => 'Projects requested to be completed',
+            'type' => 'type',
+            'nombre' => 'number',
+        ]
+    ];
+
+?>
