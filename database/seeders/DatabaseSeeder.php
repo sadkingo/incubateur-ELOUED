f44123af8d575f7017ba81d92861374f84491a92
sadkingo <hker0004@gmail.com>
2025-01-11 03:10:05 +0100

    Changed logic to be more dynamic for local env and prod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,24 +8,46 @@ use Database\Seeders\StudentTableSeeder;
 use Database\Seeders\SubjectTableSeeder;
 use Database\Seeders\TeacherTableSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(30)->create();
-        $this->call([
-            // AdminTableSeeder::class,
-            // TeacherTableSeeder::class,
-            // StudentTableSeeder::class,
-           // SubjectTableSeeder::class,
-           //FacultiesTableSeeder::class,
-           DepartementsTableSeeder::class,
-        ]);
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $staticSeeders = [
+      AdminTableSeeder::class,
+      StudentsTableSeeder::class,
+    ];
+    $developmentSeeders = [];
+
+    if (App::environment('local')) {
+      $developmentSeeders = [
+        AdministrativeFilesTableSeeder::class,
+        AttendencesTableSeeder::class,
+        DepartementsTableSeeder::class,
+        CommissionsTableSeeder::class,
+        SettingsTableSeeder::class,
+        FacultiesTableSeeder::class,
+        ManagersTableSeeder::class,
+        SubjectsTableSeeder::class,
+        SupervisingTeachersTableSeeder::class,
+        CertificatesTableSeeder::class,
+        ProjectsTableSeeder::class,
+        TeacherTableSeeder::class,
+        SupervisingTeacherProjectsTableSeeder::class,
+//        sql data given does not much migration
+//        StudentGroupsTableSeeder::class,
+      ];
     }
+
+    $this->call([
+      ...$staticSeeders,
+      ...$developmentSeeders,
+    ]);
+  }
 }
